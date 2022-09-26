ab77d80fbe8a1849e5e1cc5e2f3e827fa1fd4766
ssamyne <crowszero22@gmail.com>
2022-09-26 16:58:09 +0700

    feat: initial setup finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import ApplyLayout from './components/ui/ApplyLayout';
 
 function App() {
-  return <div>welcome to verily quiz</div>;
+  const notFoundPage = (
+    <div>
+      <h1>Why you here.. 404!!</h1>
+    </div>
+  );
+
+  return (
+    <ApplyLayout>
+      <Routes>
+        <Route path='/' element={<div>welcome to verily quiz</div>} />
+        <Route path='*' element={notFoundPage} />
+      </Routes>
+    </ApplyLayout>
+  );
 }
 
 export default App;
